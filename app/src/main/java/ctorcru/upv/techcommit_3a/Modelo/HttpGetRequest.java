89ca353d5b72905e4a3f86a32c7106a0ddedfee5
Roberto Matilla Augustinus <robertomatillaaugustinus@gmail.com>
2023-01-02 00:20:01 +0100

    Implementada clase que realiza el método GET con HTTP para obtener la información de la API.

---
+++ app/src/main/java/ctorcru/upv/techcommit_3a/Modelo/HttpGetRequest.java
@@ -0,0 +1,58 @@
+package ctorcru.upv.techcommit_3a.Modelo;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpGetRequest extends AsyncTask<String, Void, String> {
+    public static final String REQUEST_METHOD = "GET";
+    public static final int READ_TIMEOUT = 15000;
+    public static final int CONNECTION_TIMEOUT = 15000;
+
+    @Override
+    protected String doInBackground(String... params) {
+        String stringUrl = params[0];
+        String result;
+        String inputLine;
+
+        try {
+            //Crear una nueva conexión
+            URL myUrl = new URL(stringUrl);
+            HttpURLConnection connection =(HttpURLConnection) myUrl.openConnection();
+
+            //Configurar la conexión
+            connection.setRequestMethod(REQUEST_METHOD);
+            connection.setReadTimeout(READ_TIMEOUT);
+            connection.setConnectTimeout(CONNECTION_TIMEOUT);
+
+            //Conectar a la URL
+            connection.connect();
+
+            //Leer el resultado de la conexión
+            InputStreamReader streamReader = new InputStreamReader(connection.getInputStream());
+            BufferedReader reader = new BufferedReader(streamReader);
+            StringBuilder stringBuilder = new StringBuilder();
+            while((inputLine = reader.readLine()) != null){
+                stringBuilder.append(inputLine);
+            }
+            reader.close();
+            streamReader.close();
+
+            //Convertir el resultado a cadena y devolverlo
+            result = stringBuilder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            result = null;
+        }
+        return result;
+    }
+
+    protected void onPostExecute(String result){
+        super.onPostExecute(result);
+    }
+}
+
